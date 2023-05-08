c9dfaf813ebd1b151f476310852d8abd968f5240
jamiesoconnell <jamieoconnell6@gmail.com>
2023-05-08 10:07:28 -0400

    Codewars august 5

---
+++ 7 kyu/friend-shout-out.js
@@ -0,0 +1,15 @@
+// 'Shout' phrase:
+// Since in the example, the difference between the friends is 6 (F's index is 15 & that of Y's is 9), the shout phrase becomes 'Oiii F!'
+
+// Consider the following differences and its output:
+// Difference is 4 => 'Oii F!'
+// 2 => 'Oi F!'
+// 9 => 'Oiiiii F!'
+// 0 => 'Oi F!'
+// 1 => 'Oi F!'
+// 20 => 'Oiiiiiiiiii F!'
+// Some guides for coding
+// Y will always be on the left and F will be on the right.
+// The number of 'i's in the output must be >= 1
+// All the inputs are valid.
+
