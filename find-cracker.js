0e87c153446cb861f7f1340a4c5fdf87d95c52ee
jaggedges <128747065+jaggedges@users.noreply.github.com>
2023-06-05 10:09:10 -0400

    june5

---
+++ find-cracker.js
@@ -0,0 +1,68 @@
+// Someone was hacking the score. Each student's record is given as an array The objects in the array are given again as an array of scores for each name and total score. ex>
+
+// var array = [
+//   ["name1", 445, ["B", "A", "A", "C", "A", "A"]],
+//   ["name2", 110, ["B", "A", "A", "A"]],
+//   ["name3", 200, ["B", "A", "A", "C"]],
+//   ["name4", 200, ["A", "A", "A", "A", "A", "A", "A"]]
+// ];
+// The scores for each grade is:
+
+// A: 30 points
+// B: 20 points
+// C: 10 points
+// D: 5 points
+// Everything else: 0 points
+// If there are 5 or more courses and all courses has a grade of B or above, additional 20 points are awarded. After all the calculations, the total score should be capped at 200 points.
+
+// Returns the name of the hacked name as an array when scoring with this rule.
+
+// var array = [
+//   ["name1", 445, ["B", "A", "A", "C", "A", "A"]],     // name1 total point is over 200 => hacked
+//   ["name2", 110, ["B", "A", "A", "A"]],               // name2 point is right
+//   ["name3", 200, ["B", "A", "A", "C"]],               // name3 point is 200 but real point is 90 => hacked
+//   ,
+//   ["name4", 200, ["A", "A", "A", "A", "A", "A", "A"]] // name4 point is right
+// ];
+
+// return ["name1", "name3"];
+
+function findHack(arr) {
+    const points = {
+      A: 30,
+      B: 20,
+      C: 10,
+      D: 5,
+    };
+  
+    const hackedNames = [];
+  
+    for (let i = 0; i < arr.length; i++) {
+      const name = arr[i][0];
+      const totalPoints = arr[i][1];
+      const grades = arr[i][2];
+  
+      let calculatedPoints = 0;
+      let numBOrAbove = 0;
+  
+      for (let j = 0; j < grades.length; j++) {
+        const grade = grades[j];
+        if (points.hasOwnProperty(grade)) {
+          calculatedPoints += points[grade];
+          if (grade === "B" || grade === "A") {
+            numBOrAbove++;
+          }
+        }
+      }
+  
+      if (grades.length >= 5 && numBOrAbove === grades.length) {
+        calculatedPoints += 20;
+      }
+  
+      if (calculatedPoints > 200 || calculatedPoints !== totalPoints) {
+        hackedNames.push(name);
+      }
+    }
+  
+    return hackedNames;
+  }
