68a8dcd5bf65444874eaf01837ddeb07ba7ec774
jaggedges <128747065+jaggedges@users.noreply.github.com>
2023-06-13 12:30:54 -0400

    June 13

--- calculate-average.js
+++
@@ -1,7 +0,0 @@
-// Write a function which calculates the average of the numbers in a given list.
-
-// Note: Empty arrays should return 0.
-
-var find_average = (array) => {  
-    return array.length === 0 ? 0 : array.reduce((acc, ind)=> acc + ind, 0)/array.length
-  }
--- remove-duplicate.js
+++
@@ -1,7 +0,0 @@
-// Define a function that removes duplicates from an array of numbers and returns it as a result.
-
-// The order of the sequence has to stay the same.
-
-function distinct(a) {
-    return Array.from(new Set(a));
-  }
