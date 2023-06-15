17c3b510f5d541d2977ba4ec74848d8c11d8e41f
jaggedges <128747065+jaggedges@users.noreply.github.com>
2023-06-15 10:13:24 -0400

    June15

---
+++ 7 kyu/sorting-arrays.js
@@ -0,0 +1,26 @@
+// Given two arrays, a1 and a2, sort the elements of a2 based on the the index of the word in a1 that begins with the same letter.
+
+// Example 1
+// a1 = ['giraffe', 'orangutan', 'impala', 'elephant', 'rhino']
+// a2 = ['rattlesnake', 'eagle', 'geko', 'iguana', 'octopus']
+
+// returns ['geko', 'octopus', 'iguana', 'eagle', 'rattlesnake']
+
+
+function sortArray(a1, a2) {
+  let orderedArray = [];
+
+  for (let i = 0; i < a1.length; i++) {
+    let firstLetter1 = a1[i][0];
+
+    for (let j = 0; j < a2.length; j++) {
+      let firstLetter2 = a2[j][0];
+      
+      if (firstLetter1 === firstLetter2) {
+        orderedArray.push(a2[j]);
+      }
+    }
+  }
+  
+  return orderedArray;
+}
