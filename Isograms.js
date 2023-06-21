3f961000aacc984f0c86b67466097473cc7114b5
jamiesoconnell <jamieoconnell6@gmail.com>
2023-06-21 10:02:36 -0400

    June 21

---
+++ Isograms.js
@@ -0,0 +1,24 @@
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+// Example: (Input --> Output)
+
+// "Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+
+// isIsogram "Dermatoglyphics" = true
+// isIsogram "moose" = false
+// isIsogram "aba" = false
+
+function isIsogram(str) {
+    str = str.toLowerCase()
+    for (let i = 0; i < str.length; i++) {
+      let currentLetter = str[i];
+      let remainingLetters = str.slice(i + 1);
+  
+      if (remainingLetters.includes(currentLetter)) {
+        return false; // Repeating letter found, not an isogram
+      }
+    }
+  
+    return true; // No repeating letters found, it's an isogram
+  }
+  
