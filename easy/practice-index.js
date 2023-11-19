9c9bd170fe9551ddc7bc8c999712e263c83729a8
jamiesoconnell <jamieoconnell6@gmail.com>
2023-11-19 09:35:37 -0500

    leet

---
+++ easy/practice-index.js
@@ -0,0 +1,18 @@
+// Input: haystack = "sadbutsad", needle = "sad"
+// Output: 0
+// Explanation: "sad" occurs at index 0 and 6.
+// The first occurrence is at index 0, so we return 0.
+// Example 2:
+
+// Input: haystack = "leetcode", needle = "leeto"
+// Output: -1
+// Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+
+function haystack (str1, str2){
+    console.log(str1.indexOf(str2))
+  }
+  
+  haystack("sadbutsad", "sad")
+  haystack("leetcode", "leeto")
+  haystack("This is a beautiful day", "day")
