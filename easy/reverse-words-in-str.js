8e2b63e830db7e806d70b30338e29a9b0e9be857
jamiesoconnell <jamieoconnell6@gmail.com>
2023-12-02 12:32:46 -0500

    leet

---
+++ easy/reverse-words-in-str.js
@@ -0,0 +1,10 @@
+function reversed(str){
+    return str
+    .split(' ')
+    .reverse()
+    .join(' ')
+  }
+  
+  
+  console.log(reversed("Today is a beautiful day"))
+  console.log(reversed("Monkeys eat bananas"))
