691c39bdc09d0e4bd4c84f90b5675c4f245103bf
jamiesoconnell <jamieoconnell6@gmail.com>
2023-12-05 13:44:36 -0500

    leet

---
+++ easy/length-last-word.js
@@ -0,0 +1,9 @@
+
+function wordLength(str){
+    let words = str.split(' ')
+  
+     return words[words.length - 1].length;
+    
+  }
+  
+  console.log(wordLength("what is the length of the last words"))
