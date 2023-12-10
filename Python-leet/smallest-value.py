b30cfb191a1e926c03814c601a1879f2f36d6d84
jamiesoconnell <jamieoconnell6@gmail.com>
2023-12-10 09:37:15 -0500

    codewars

--- Python-leet/smallest-value.py
+++ Python-leet/smallest-value.py
@@ -2,6 +2,8 @@ Write a function that can return the smallest value of an array or the index of 
 
 Assume the first parameter will always be an array filled with at least 1 number and no duplicates. Assume the second parameter will be a string holding one of two values: 'value' and 'index'.
 
+
+
 min([1,2,3,4,5], 'value') // => 1
 min([1,2,3,4,5], 'index') // => 0
 
