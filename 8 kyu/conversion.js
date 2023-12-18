7da7f1a8032dab355d5aa6f188eb48b98a394664
jamiesoconnell <jamieoconnell6@gmail.com>
2023-12-18 12:27:01 -0600

    codewars

--- 8 kyu/conversion.js
+++ 8 kyu/conversion.js
@@ -1,3 +1,16 @@
 function usdcny(usd) {
     return (usd * 6.75)+ " Chinese Yuan"
   }
+
+
+  //fixed code :
+  
+  function usdcny(usd) {
+    // Calculate the Chinese Yuan
+    let yuan = usd * 6.75;
+  
+    // Format the result with two decimal places
+    let formattedYuan = yuan.toFixed(2);
+  
+    return formattedYuan + " Chinese Yuan";
+  }
