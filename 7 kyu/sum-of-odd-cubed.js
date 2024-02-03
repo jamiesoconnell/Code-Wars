94ba60b029a090eab793da7a49fcfc3dc7c1b17e
jamiesoconnell <jamieoconnell6@gmail.com>
2024-02-03 10:27:30 -0500

    codewars

---
+++ 7 kyu/sum-of-odd-cubed.js
@@ -0,0 +1,12 @@
+// Find the sum of the odd numbers within an array, after cubing the initial integers. The function should return undefined if any of the values aren't numbers.
+
+
+function cubeOdd(arr) {
+    var sum = 0;
+    for (var i = 0; i <arr.length; i++){
+      var cubed = (arr[i]*arr[i]*arr[i]);
+      if (isNaN(cubed)) return undefined;
+      if(cubed % 2 != 0 )sum += cubed;
+    }
+    return sum;
+  }
