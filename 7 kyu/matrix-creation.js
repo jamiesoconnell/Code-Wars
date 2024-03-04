39ddfd07b1102c4445b053052fb57d464493a63e
jamiesoconnell <jamieoconnell6@gmail.com>
2024-03-04 09:41:15 -0500

    codewars

---
+++ 7 kyu/matrix-creation.js
@@ -0,0 +1,29 @@
+// Create an identity matrix of the specified size( >= 0).
+
+// Some examples:
+
+// (1)  =>  [[1]]
+
+// (2) => [ [1,0],
+//          [0,1] ]
+
+//        [ [1,0,0,0,0],
+//          [0,1,0,0,0],
+// (5) =>   [0,0,1,0,0],
+//          [0,0,0,1,0],
+//          [0,0,0,0,1] ]   
+
+
+function getMatrix(number) {
+    let matrix = [];
+    for (let i = 0; i < number; i++) {
+      let row = [];
+      for (let j = 0; j < number; j++) {
+        // If the current row index equals the column index, set the value to 1, otherwise set it to 0
+        row.push(i === j ? 1 : 0);
+      }
+      matrix.push(row);
+    }
+    return matrix;
+  }
+  
